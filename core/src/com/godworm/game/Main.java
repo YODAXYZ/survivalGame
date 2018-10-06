b7f48cad082c6013d0de5149629c99cee403ed39
frohenk <frohenkwork@gmail.com>
2018-10-06 15:43:35 +0300

    Formatted files, +started making loading screen

--- core/src/com/godworm/game/Main.java
+++ core/src/com/godworm/game/Main.java
@@ -1,12 +1,20 @@
 package com.godworm.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 import com.godworm.game.screens.MainMenuScreen;
 
 public class Main extends Game {
 
-	@Override
-	public void create() {
-		setScreen(new MainMenuScreen(this));
-	}
+    private AssetManager assetManager;
+
+    @Override
+    public void create() {
+        assetManager = new AssetManager();
+        setScreen(new MainMenuScreen(this));
+    }
+
+    public AssetManager getAssetManager() {
+        return assetManager;
+    }
 }
--- core/src/com/godworm/game/screens/LoadingScreen.java
+++ core/src/com/godworm/game/screens/LoadingScreen.java
@@ -1,11 +1,33 @@
 package com.godworm.game.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.godworm.game.Main;
 
 public class LoadingScreen implements Screen {
+    private Main main;
+    private AssetManager assetManager;
+    private Screen screen;
+    private Stage stage;
+
+    public LoadingScreen(Main main, AssetManager assetManager, Screen screen) {
+        this.main = main;
+        this.assetManager = assetManager;
+        this.screen = screen;
+    }
+
     @Override
     public void show() {
-        // TODO Auto-generated method stub
+        stage = new Stage(new ExtendViewport(640, 480));
+        Gdx.input.setInputProcessor(stage);
+        Table table = new Table();
+        table.setFillParent(true);
+        table.setDebug(true);
+        stage.addActor(table);
     }
 
     @Override
