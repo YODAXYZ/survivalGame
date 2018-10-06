2bdb2b6dca5d080cff05907887e058a5af44ed8c
frohenk <frohenkwork@gmail.com>
2018-10-06 16:22:45 +0300

    Added MainScreen, from which all screens will extend from

--- core/src/com/godworm/game/screens/LoadingScreen.java
+++ core/src/com/godworm/game/screens/LoadingScreen.java
@@ -1,29 +1,17 @@
 package com.godworm.game.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.godworm.game.Main;
 
-public class LoadingScreen implements Screen {
-    private Main main;
-    private AssetManager assetManager;
-    private Screen screen;
-    private Stage stage;
+public class LoadingScreen extends MainScreen {
 
-    public LoadingScreen(Main main, AssetManager assetManager, Screen screen) {
-        this.main = main;
-        this.assetManager = assetManager;
-        this.screen = screen;
+    public LoadingScreen(Main main) {
+        super(main);
     }
 
     @Override
     public void show() {
-        stage = new Stage(new ExtendViewport(640, 480));
-        Gdx.input.setInputProcessor(stage);
+        super.show();
         Table table = new Table();
         table.setFillParent(true);
         table.setDebug(true);
@@ -32,31 +20,7 @@ public class LoadingScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        // TODO Auto-generated method stub
+        super.render(delta);
     }
 
-    @Override
-    public void resize(int width, int height) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void pause() {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void resume() {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void hide() {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    public void dispose() {
-        // TODO Auto-generated method stub
-    }
 }
--- core/src/com/godworm/game/screens/MainMenuScreen.java
+++ core/src/com/godworm/game/screens/MainMenuScreen.java
@@ -2,29 +2,23 @@ package com.godworm.game.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.godworm.game.Main;
 
-public class MainMenuScreen implements Screen {
-    private Main parent;
-    private Stage stage;
+public class MainMenuScreen extends MainScreen {
 
-    public MainMenuScreen(Main box2dTutorial) {
-        parent = box2dTutorial;
-        /// create stage and set it as input processor
+
+    public MainMenuScreen(Main main) {
+        super(main);
     }
 
     @Override
     public void show() {
-        stage = new Stage(new ExtendViewport(640, 480));
-        Gdx.input.setInputProcessor(stage);
+        super.show();
         // Create a table that fills the screen. Everything else will go inside this table.
         Table table = new Table();
         table.setFillParent(true);
@@ -32,7 +26,7 @@ public class MainMenuScreen implements Screen {
         stage.addActor(table);
 
         // temporary until we have asset manager in
-        Skin skin = parent.getAssetManager().get("skin/craftacular-ui.json", Skin.class);
+        Skin skin = assetManager.get("skin/craftacular-ui.json", Skin.class);
 
         //create buttons
         TextButton newGame = new TextButton("New Game", skin);
@@ -64,7 +58,7 @@ public class MainMenuScreen implements Screen {
         preferences.addListener(new ChangeListener() {
             @Override
             public void changed(ChangeEvent event, Actor actor) {
-                parent.setScreen(new Screen() {
+                main.setScreen(new Screen() {
                     @Override
                     public void show() {
 
@@ -104,47 +98,4 @@ public class MainMenuScreen implements Screen {
         });
 
     }
-
-    @Override
-    public void render(float delta) {
-        // clear the screen ready for next set of images to be drawn
-        Gdx.gl.glClearColor(0f, 0f, 0f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        // tell our stage to do actions and draw itself
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
-        stage.draw();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        // change the stage's viewport when teh screen size is changed
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void pause() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void resume() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void hide() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void dispose() {
-        // dispose of assets when not needed anymore
-        stage.dispose();
-    }
-
-
 }
